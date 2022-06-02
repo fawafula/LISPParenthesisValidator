454a9e12f6f036fcaa589b6e036033606700ab78
Felix Wafula <felix.a.wafula@gmail.com>
2022-06-01 20:04:46 -0600

    Added ValidateLISPCode method.

---
+++ LISPParenthesisValidator.Tests/Validates_LISP_Parentheses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LISPParenthesisValidator.Tests
+{
+    public class Validates_LISP_Parentheses
+    {
+        [Fact]
+        public void ValidLISP()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
--- LISPParenthesisValidator/Program.cs
+++ LISPParenthesisValidator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LISPParenthesisValidator
 {
@@ -6,7 +8,39 @@ namespace LISPParenthesisValidator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to LISP validator application!");
+
+            var lispCodeIsvalid = ValidateLISPCode(args);
+
+            if (lispCodeIsvalid)
+            {
+
+                Console.WriteLine("LISP code paentheses are valid.");
+            }
+            else
+            {
+                Console.WriteLine("LISP code parentheses validation failed.");
+            }
+
+            Console.WriteLine("Application closed.");
+
+        }
+
+        private static bool ValidateLISPCode(string[] lispCodargs)
+        {
+            var looongString = string.Join(',', lispCodargs);
+           
+            var openParenthesesList = looongString.Where(c => c == '(');
+
+            var closeParenthesesList = looongString.Where(c => c == ')');
+                     
+
+            if (openParenthesesList.ToList().Count == closeParenthesesList.ToList().Count)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
